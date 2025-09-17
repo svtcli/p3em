cc59e86de003bfc764379e5375f2fb72a629e7fa
di52vum <Salvatore.Cielo@lrz.de>
2025-09-17 11:25:28 +0200

    try

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,13 +4,11 @@
 int main() {
     std::thread monitorThread(launchScriptAndMonitor);
     std::this_thread::sleep_for(std::chrono::seconds(2)); // Wait a bit for init
-
     // Simulate usage
     for (int i = 0; i < 10; ++i) {
         std::cout << "Latest value: " << getLatestValue() << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
-
     // Cleanup
     if (scriptPid > 0) killpg(scriptPid, SIGTERM); // Kill entire process group
     monitorThread.join();
