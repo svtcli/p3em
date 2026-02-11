6d7472b879ab6421efe2c629d6348ba5bc791de8
di52vum <Salvatore.Cielo@lrz.de>
2026-02-11 11:30:36 +0100

    More debug

--- meters/xpuSmi.sh
+++ meters/xpuSmi.sh
@@ -15,10 +15,10 @@ SEC=${1:-0.1}
 MSEC=$(echo $SEC | awk '{print $1 * 1000}')
 
 ##- Get GPUs per node
-GPN=4 #$(( 1 + $(xpu-smi discovery --dump 1 2>/dev/null | tail -n 1)))
+GPN=$(( 1 + $(xpu-smi discovery --dump 1 2>/dev/null | tail -n 1)))
 
 ##- Zero it (optional)
-Z=$(xpu-smi dump -m 8 -n 1 --file /dev/stdout 2>/dev/null | awk -v gpn=$GPN '(NR>1){sum+=$3+0}END{print sum}')
-echo $Z > zet.dat
+Z=$(unbuffer xpu-smi dump -m 8 -n 1 --file /dev/stdout 2>/dev/null | awk -v gpn=$GPN '(NR>1){sum+=$3+0}END{print sum}')
+
 unbuffer xpu-smi dump -m 8 --ims $MSEC --file /dev/stdout 2>/dev/null | \
   awk -v gpn=$GPN -v z=$Z '{s+=$3+0}((NR+2)%gpn)==0 {printf "%d\n", s-z; s=0}'
