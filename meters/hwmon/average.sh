e2735de8f7365391073083a762dbcc2bb248540b
Salvatore Cielo <di52vum@x8000c1s0b0n0.hsn.simba.lrz.de>
2026-02-23 10:48:25 +0100

    Updated meters/hwmon/ meters to accept multiple devices

--- meters/hwmon/average.sh
+++ meters/hwmon/average.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #- hwmon device number (default: all)
-x=${1:-"*"}
-MPATH=/sys/class/hwmon/hwmon${x}/device/power1_average
+x=("${@:-*}")
+MPATH=()
+for val in "${x[@]}"; do
+  MPATH+=("/sys/class/hwmon/hwmon${val}/device/power1_average")
+done
 
 #- Time duration
-SEC=$(cat ${MPATH}_interval | awk '{print $1/1000}')
+SEC=$(cat ${MPATH}_interval | awk '{print $1/1000}')  #| awk '(NR==1){print $1/1000}'
 
 #- Read in loop and process with awk
 while true; do
-  cat $MPATH
+  cat ${MPATH[@]}
   sleep $SEC
 done | awk -v sec=$SEC '{sum += ($1*sec/1.0E6); print sum}'
-
--- meters/hwmon/instant.sh
+++ meters/hwmon/instant.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #- hwmon device number (default: all)
-x=${1:-"*"}
-MPATH=/sys/class/hwmon/hwmon${x}/device/power1_instant
+x=("${@:-*}")
+MPATH=()
+for val in "${x[@]}"; do
+  MPATH+=("/sys/class/hwmon/hwmon${val}/device/power1_instant")
+done
 
 #- Time duration
 SEC=${2:-0.1}
 
 #- Read in loop and process with awk
 while true; do
-  cat $MPATH
+  cat ${MPATH[@]}
   sleep $SEC
 done | awk -v sec=$SEC '{sum += ($1*sec/1.0E6); print sum}'
